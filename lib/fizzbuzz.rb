39f176703801af1494e5f4ea5dc28bdc22024297
Matt Dawson <mattdawson18@gmail.com>
2021-03-22 11:08:47 +0000

    fizbuzz passses expected tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,4 +2,9 @@ def fizzbuzz(num)
     return 'fizzbuzz' if num % 15 == 0
     return 'fizz' if num % 3 == 0
     return 'buzz' if num % 5 == 0
+    return num
 end
+
+puts fizzbuzz(10)
+puts fizzbuzz(15)
+puts fizzbuzz(100)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,24 @@ describe "fizzbuzz" do
     it "returns fizzbuzz when passed 5" do
         expect(fizzbuzz(15)).to eq "fizzbuzz"
     end
+
+    it "returns the number otherwise" do
+        expect(fizzbuzz(7)).to eq 7
+    end
+
+    it "returns fizz when passed 3" do
+        expect(fizzbuzz(27)).to eq "fizz"
+    end
+
+    it "returns buzz when passed 5" do
+        expect(fizzbuzz(20)).to eq "buzz"
+    end
+
+    it "returns fizzbuzz when passed 5" do
+        expect(fizzbuzz(45)).to eq "fizzbuzz"
+    end
+
+    it "returns the number otherwise" do
+        expect(fizzbuzz(4)).to eq 4
+    end
 end
